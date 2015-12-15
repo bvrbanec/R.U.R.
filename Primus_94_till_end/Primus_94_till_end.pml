326a21b642d7f75cef41478445cc055b730e8c0b
kovimir <k-ova@windowslive.com>
2015-12-15 16:03:45 +0100

    do kraja python za cieli epilog, i spojena i konacna verzija primusa
    primus je spojen josipovo i od dečkiju i sad je potpuno kompletan, s
    ovime treba raditi i usavrašavati

---
+++ Primus_94_till_end/Primus_94_till_end.pml
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="Primus_94_till_end" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="97primus1" src="97primus1.wav" />
+        <File name="97primus2" src="97primus2.wav" />
+        <File name="97primus3" src="97primus3.wav" />
+        <File name="97primus4" src="97primus4.wav" />
+        <File name="97primus5" src="97primus5.wav" />
+        <File name="97primus6" src="97primus6.wav" />
+        <File name="94primus6" src="Primus/94primus6.wav" />
+        <File name="94primus7" src="Primus/94primus7.wav" />
+        <File name="95primus1" src="Primus/95primus1.wav" />
+        <File name="95primus2" src="Primus/95primus2.wav" />
+        <File name="95primus4" src="Primus/95primus4.wav" />
+        <File name="95primus5" src="Primus/95primus5.wav" />
+        <File name="95primus6" src="Primus/95primus6.wav" />
+        <File name="95primus7" src="Primus/95primus7.wav" />
+        <File name="96primus1" src="Primus/96primus1.wav" />
+        <File name="96primus2" src="Primus/96primus2.wav" />
+        <File name="96primus3" src="Primus/96primus3.wav" />
+        <File name="96primus4" src="Primus/96primus4.wav" />
+        <File name="96primus5" src="Primus/96primus5.wav" />
+        <File name="97primus1" src="Primus/97primus1.wav" />
+        <File name="97primus2" src="Primus/97primus2.wav" />
+        <File name="97primus3" src="Primus/97primus3.wav" />
+        <File name="97primus4" src="Primus/97primus4.wav" />
+        <File name="97primus5" src="Primus/97primus5.wav" />
+        <File name="97primus6" src="Primus/97primus6.wav" />
+        <File name="98primus1" src="Primus/98primus1.wav" />
+        <File name="98primus2" src="Primus/98primus2.wav" />
+        <File name="98primus3" src="Primus/98primus3.wav" />
+        <File name="98primus4" src="Primus/98primus4.wav" />
+        <File name="99primus1" src="Primus/99primus1.wav" />
+        <File name="99primus2" src="Primus/99primus2.wav" />
+        <File name="94primus6" src="94primus6.wav" />
+        <File name="94primus7" src="94primus7.wav" />
+        <File name="95primus1" src="95primus1.wav" />
+        <File name="95primus2" src="95primus2.wav" />
+        <File name="95primus4" src="95primus4.wav" />
+        <File name="95primus5" src="95primus5.wav" />
+        <File name="95primus6" src="95primus6.wav" />
+        <File name="95primus7" src="95primus7.wav" />
+        <File name="96primus1" src="96primus1.wav" />
+        <File name="96primus2" src="96primus2.wav" />
+        <File name="96primus3" src="96primus3.wav" />
+        <File name="96primus4" src="96primus4.wav" />
+        <File name="96primus5" src="96primus5.wav" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+</Package>
--- RUR.py
+++ RUR.py
@@ -1,108 +1,243 @@
 import socket
+import time
+try:
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serversocket.bind(('192.168.1.110', 8072))
+    serversocket.listen(2) # become a server socket, maximum 5 connections
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serversocket.bind(('192.168.1.111', 8072))
-serversocket.listen(5) # become a server socket, maximum 5 connections
+    
+    
+    time.sleep(5)
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+    time.sleep(5)
+    helena, address1 = serversocket.accept()        ##pocetna spajanja
+    print(helena,address1)
+    
+    x=raw_input()       ##kreni s pretstavom
+    helena.send('helena')           #Primus look
+    time.sleep(1)
+    helena, address1 = serversocket.accept()
+    print(helena,address1)
 
-helena, address1 = serversocket.accept()        ##pocetna spajanja
-print(helena,address1)
-primus, address2 = serversocket.accept()
-print(primus,address2)
-x=raw_input()       ##kreni s pretstavom
+    helena.recv(512)
+    primus.send('whats up')         # what
+    primus, address2 = serversocket.accept()
 
-helena.send('helena')           #What could happen to us, Primus? 
-print(helena.recv(512))
-helena, address1 = serversocket.accept()       
-print(helena,address1)
-                                #Alquist prica
-x=raw_input()  
-primus.send('bok')              #The Robot Primus.
-primus, address2 = serversocket.accept()
-print(primus,address2)          #i ceka
-                                #Alquist:what..
-x=raw_input()
-helena.send('helena')           #Robotess helena 
-print(helena.recv(512))
-helena, address1 = serversocket.accept()       
-print(helena,address1)
-                                #Alguist: Turn around
-x=raw_input()
-primus.send('bok')              #Sir, do not frighten her.
-primus, address2 = serversocket.accept()
-print(primus,address2)
+    primus.recv(512)
+    helena.send('helena')           #the sun is rising
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
 
-                                #Alguist: what?
-x=raw_input()
-primus.send('bok')              #2years ago.
-primus, address2 = serversocket.accept()
-print(primus,address2)
-                                #Aluist: By dr.Gall?
-x=raw_input()
-primus.send('bok')              #Yes Like me
-primus, address2 = serversocket.accept()
-print(primus,address2)
-                                #Aluist: Laugter Lughetr?
-x=raw_input()
-primus.send('bok')              #Why
-primus, address2 = serversocket.accept()
-print(primus,address2)
-                                #Aluist: I wich ti experiment on her?
-x=raw_input()
-primus.send('bok')              #Uppon heena?
-primus, address2 = serversocket.accept()
-print(primus,address2)
-                                #Aluist: Ofcourse?
-x=raw_input()
-primus.send('bok')              #If you do i will kill you
-primus, address2 = serversocket.accept()
-print(primus,address2)
-                                #Aluist: Kill me
-x=raw_input()
-primus.send('bok')              #Sir take me
-primus, address2 = serversocket.accept()
-print(primus,address2)
-primus.recv(512) #i cekaj sljedeci
+    helena.recv(512)
+    primus.send('whats up')         # i believe this is themost
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
 
-helena.send('oj')               #no no, you shall not
-helena, address1 = serversocket.accept()
-print(helena,address1)
-                            #Aluist: Whait girl ! wait
-x=raw_input()
-primus.send('bok')              #Not without her
-primus, address2 = serversocket.accept()
-print(primus,address2)
-                            #Aluist: very vell
-x=raw_input()
-helena.send('bok')              #Primus primus
-helena, address1 = serversocket.accept()
-print(helena,address1)
-                            #Aluist: child child you can weep
-x=raw_input()
-helena.send('bok')              #Iwill go myself
-helena, address1 = serversocket.accept()
-print(helena,address1)
-                            #Aluist: where??
-helena.send('helena')      ## Helena:In there to be cut.  Let me pass, Primus! Let me pass! 
-helena, address1 = serversocket.accept()
-print(helena,address1)
+    primus.recv(512)
+    helena.send('helena')           #do come here
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
 
-helena.recv(512)        #you shoul not go
-primus.send('bok')
-primus, address2 = serversocket.accept()
-print(primus,address2)
+    helena.recv(512)
+    primus.send('whats up')         # in a moment
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
 
-primus.recv(512)        #if you go in there
-helena.send('kreni')
-helena.recv(512)
-helena, address1 = serversocket.accept()
-print(helena,address1)
-print(helena.recv(512))
-primus.send('bok')
-primus, address2 = serversocket.accept()
-print(primus,address2)
-x=raw_input()
-primus.send('bok')
+    primus.recv(512)
+    helena.send('helena')           #oh, primua, dont bother
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
 
+    helena.recv(512)
+    primus.send('whats up')         #what is it?
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
 
-serversocket.close()
+    primus.recv(512)
+    helena.send('helena')           #see how beautiful
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+
+    helena.recv(512)
+    primus.send('whats up')         #why
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+    primus.recv(512)
+    helena.send('helena')           #i dont know, i feel
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+
+    helena.recv(512)
+    primus.send('whats up')         #do you not sometimes feel
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+    primus.recv(512)
+    helena.send('helena')           #in your sleep?
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+
+    helena.recv(512)
+    primus.send('whats up')         #yes, we spoke
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+    primus.recv(512)
+    helena.send('helena')           #what about
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+
+    helena.recv(512)
+    primus.send('whats up')         #i did not understand it myself
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+    primus.recv(512)
+    helena.send('helena')           #i, too, have found a place
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+
+    helena.recv(512)
+    primus.send('whats up')         #what did you find there
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+    primus.recv(512)
+    helena.send('helena')           #a cottage and a garde
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+
+    helena.recv(512)
+    primus.send('whats up')         #i do not know
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+    primus.recv(512)
+    helena.send('helena')           #what, primus
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+
+    helena.recv(512)
+    primus.send('whats up')         #you are beatufful,
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+    primus.recv(512)
+    helena.send('helena')           #iam i beatufilf
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+
+    helena.recv(512)
+    primus.send('whats up')         #it is you who run away
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+    primus.recv(512)
+    helena.send('helena')           #your hair is mussed. i will
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+
+    helena.recv(512)
+    primus.send('whats up')         #do you not sometimes feel
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+    primus.recv(512)
+    helena.send('helena')           #What could happen to us, Primus? 
+    print(helena.recv(512))
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+                                    #Alquist prica
+    x=raw_input()  
+    primus.send('bok')              #The Robot Primus.
+    primus, address2 = serversocket.accept()
+    print(primus,address2)          #i ceka
+                                    #Alquist:what..
+    x=raw_input()
+    helena.send('helena')           #Robotess helena 
+    print(helena.recv(512))
+    helena, address1 = serversocket.accept()       
+    print(helena,address1)
+                                    #Alguist: Turn around
+    x=raw_input()
+    primus.send('bok')              #Sir, do not frighten her.
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+                                    #Alguist: what?
+    x=raw_input()
+    primus.send('bok')              #2years ago.
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+                                    #Aluist: By dr.Gall?
+    x=raw_input()
+    primus.send('bok')              #Yes Like me
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+                                    #Aluist: Laugter Lughetr?
+    x=raw_input()
+    primus.send('bok')              #Why
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+                                    #Aluist: I wich ti experiment on her?
+    x=raw_input()
+    primus.send('bok')              #Uppon heena?
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+                                    #Aluist: Ofcourse?
+    x=raw_input()
+    primus.send('bok')              #If you do i will kill you
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+                                    #Aluist: Kill me
+    x=raw_input()
+    primus.send('bok')              #Sir take me
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+    primus.recv(512) #i cekaj sljedeci
+
+    helena.send('oj')               #no no, you shall not
+    helena, address1 = serversocket.accept()
+    print(helena,address1)
+                                #Aluist: Whait girl ! wait
+    x=raw_input()
+    primus.send('bok')              #Not without her
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+                                #Aluist: very vell
+    x=raw_input()
+    helena.send('bok')              #Primus primus
+    helena, address1 = serversocket.accept()
+    print(helena,address1)
+                                #Aluist: child child you can weep
+    x=raw_input()
+    helena.send('bok')              #Iwill go myself
+    helena, address1 = serversocket.accept()
+    print(helena,address1)
+                                #Aluist: where??
+    helena.send('helena')      ## Helena:In there to be cut.  Let me pass, Primus! Let me pass! 
+    helena, address1 = serversocket.accept()
+    print(helena,address1)
+
+    helena.recv(512)        #you shoul not go
+    primus.send('bok')
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+
+    primus.recv(512)        #if you go in there
+    helena.send('kreni')
+    helena.recv(512)
+    helena, address1 = serversocket.accept()
+    print(helena,address1)
+    print(helena.recv(512))
+    primus.send('bok')
+    primus, address2 = serversocket.accept()
+    print(primus,address2)
+    x=raw_input()
+    primus.send('bok')
+
+finally:
+    serversocket.close()
